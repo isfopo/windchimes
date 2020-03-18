4f8552194ecd4ecd4b0d5f0b05251381f134d5bf
isfopo <55164207+isfopo@users.noreply.github.com>
2020-03-18 17:57:51 -0400

    removes useless old code

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,21 +4,9 @@ import logo from '../logo.svg';
 
 import '../css/App.css';
 
-import Chimes from './Chimes';
+import { Chimes } from './Chimes';
 
 class App extends Component {
-
-  state = {
-    chimeNotes: []
-  }
-
-  addChime = () => {
-    const array = [...this.state.chimeNotes].push(prompt("Please enter a note").toUpperCase());
-    
-    this.setState({
-        chimeNotes: array
-    })
-  }
 
   render() {
     return (
@@ -27,10 +15,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />  
 
           <div className="chime-wrapper">
-            <Chimes 
-              chimeNotes={this.state.chimeNotes}
-              addChime={this.addChime}
-            />
+            <Chimes />
           </div>
 
         </header>
