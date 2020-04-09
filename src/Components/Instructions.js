6fc65484e0901a07420900c79a8d5c43815f3f97
isfopo <55164207+isfopo@users.noreply.github.com>
2020-04-09 11:33:51 -0400

    adds instructions

---
+++ src/Components/Instructions.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Instructions = () => {
+    return (
+        <div className="instructions" >
+            <p>Choose from menu above to create a new chime.</p>
+            <p>Swipe over a chime start ringing.</p>
+            <p>Click chime to remove it.</p>
+            <p>Choose from a number of preset scales and materials.</p>
+        </div>
+    )
+}
